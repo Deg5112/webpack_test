4fbc74eb87bdaefa5b198afdabc5c86b9a04b92b
davidgoodman <dgoodman@tomferry.com>
2018-06-28 23:10:02 -0700

    latest

--- resources/assets/js/vue/index.js
+++ resources/assets/js/vue/index.js
@@ -2,9 +2,5 @@ import App   from 'VueSrc/components/app';
 import testA from 'VueSrc/components/testA/testA.vue';
 import testB from 'VueSrc/components/testB/testB.vue';
 
-new referralVue({
-  router:     referralRouter,
-  store:      referralStore,
-  template:   '<App/>',
-  components: { App },
-}).$mount('#app');
+
+
